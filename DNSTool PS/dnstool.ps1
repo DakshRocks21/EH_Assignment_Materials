25b5c3ea0d7c53e7491f63728992d36c520495b8
ChinRayTan <167780333+ChinRayTan@users.noreply.github.com>
2025-07-12 01:28:34 +0800

    Almost done with DNSTool

--- DNSTool PS/dnstool.ps1
+++ DNSTool PS/dnstool.ps1
@@ -11,30 +11,32 @@ $dnsRecord.AddRange([BitConverter]::GetBytes([UInt16]4))
 # Type (A record)
 $dnsRecord.AddRange([BitConverter]::GetBytes([UInt16]1))
 # Version
-$dnsRecord.Add(5)
+$dnsRecord.Add(0x05)
 # Rank
 $dnsRecord.Add(240)
 # Flags
-$dnsRecord.AddRange([BitConverter]::GetBytes([UInt16]0))
+$dnsRecord.Add([byte[]](0x00, 0x00))
 # Serial
-$dnsRecord.AddRange([BitConverter]::GetBytes($DnsResult.SerialNumber))
+$dnsRecord.AddRange([BitConverter]::GetBytes([UInt32]$DnsResult.SerialNumber))
 # TTL
-$dnsRecord.AddRange([BitConverter]::GetBytes([UInt32]([System.Net.IPAddress]::HostToNetworkOrder([int]$ttl))))
+$dnsRecord.AddRange([BitConverter]::GetBytes([UInt32]$ttl))
 # Reserved
-$dnsRecord.AddRange([byte[]](0, 0, 0, 0))
+$dnsRecord.AddRange([byte[]](0x00, 0x00, 0x00, 0x00))
 # Timestamp
-$dnsRecord.AddRange([byte[]](0, 0, 0, 0))
+$dnsRecord.AddRange([byte[]](0x00, 0x00, 0x00, 0x00))
 # IP address (4 bytes)
 $dnsRecord.AddRange([System.Net.IPAddress]::Parse($SrcIp).GetAddressBytes())
 
+$dnsRecord | ForEach-Object { "{0:X2}" -f $_ }
+
 # Convert to byte array
-$dnsRecordBytes = $dnsRecord.ToArray()
+#$dnsRecordBytes = $dnsRecord.ToArray()
 
 #Design the object
 $Props = @{
     objectClass       = @("top", "dnsNode")
     distinguishedName = "DC={0},DC={1},CN=MicrosoftDNS,DC=DomainDnsZones,DC={2},DC={3}" -f $DstRecord, $DomainName, $DomainName.Split(".")[0], $DomainName.Split(".")[1]
-    dnsRecord         = $dnsRecordBytes
+    dnsRecord         = [System.Text.Encoding]::ASCII.GetString($dnsRecord)
 }
 
 #Create the object according to design
