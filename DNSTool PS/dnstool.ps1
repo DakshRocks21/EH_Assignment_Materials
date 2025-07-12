2858f7ff2c1a46bcd62280484aaeaf983bfb6a91
ChinRayTan <167780333+ChinRayTan@users.noreply.github.com>
2025-07-12 19:57:22 +0800

    FINALLY fixed DNSTool - that package was pure cancer

--- DNSTool PS/dnstool.ps1
+++ DNSTool PS/dnstool.ps1
@@ -1,6 +1,11 @@
-param([Parameter(Mandatory = $true)]$ServerIp, [Parameter(Mandatory = $true)]$DomainName, [Parameter(Mandatory = $true)]$SrcIp, [Parameter(Mandatory = $true)]$DstRecord)
+param([Parameter(Mandatory = $true)]$ServerIp, [Parameter(Mandatory = $true)]$DomainName, [Parameter(Mandatory = $true)]$SrcRecord, [Parameter(Mandatory = $true)]$DstIp)
 
-Get-LdapConnection -LdapServer $ServerIp -EncryptionType Kerberos
+Add-Type -AssemblyName System.DirectoryServices.Protocols
+
+$ldapIdentifier = New-Object System.DirectoryServices.Protocols.LdapDirectoryIdentifier($ServerIp, $null, $false, $false)
+$ldapConnection = New-Object System.DirectoryServices.Protocols.LdapConnection($ldapIdentifier)
+$ldapConnection.AuthType = [System.DirectoryServices.Protocols.AuthType]::Negotiate
+$ldapConnection.Bind()
 
 $DnsResult = Resolve-DnsName $DomainName -Type SOA
 
@@ -11,39 +16,47 @@ $dnsRecord.AddRange([BitConverter]::GetBytes([UInt16]4))
 # Type (A record)
 $dnsRecord.AddRange([BitConverter]::GetBytes([UInt16]1))
 # Version
-$dnsRecord.Add(0x05)
+$dnsRecord.Add(5)
 # Rank
 $dnsRecord.Add(240)
 # Flags
-$dnsRecord.Add([byte[]](0x00, 0x00))
+$dnsRecord.AddRange([byte[]](0x00, 0x00))
 # Serial
 $dnsRecord.AddRange([BitConverter]::GetBytes([UInt32]$DnsResult.SerialNumber))
 # TTL
-$dnsRecord.AddRange([BitConverter]::GetBytes([UInt32]$ttl))
+$ttl = [BitConverter]::GetBytes([UInt32]180)
+[void][array]::Reverse($ttl)
+$dnsRecord.AddRange($ttl)
 # Reserved
 $dnsRecord.AddRange([byte[]](0x00, 0x00, 0x00, 0x00))
 # Timestamp
 $dnsRecord.AddRange([byte[]](0x00, 0x00, 0x00, 0x00))
 # IP address (4 bytes)
-$dnsRecord.AddRange([System.Net.IPAddress]::Parse($SrcIp).GetAddressBytes())
+$dnsRecord.AddRange([System.Net.IPAddress]::Parse($DstIp).GetAddressBytes())
 
 $dnsRecord | ForEach-Object { "{0:X2}" -f $_ }
 
 # Convert to byte array
 #$dnsRecordBytes = $dnsRecord.ToArray()
 
+$distinguishedName = "DC={0},DC={1},CN=MicrosoftDNS,DC=DomainDnsZones,DC={2},DC={3}" -f $SrcRecord, $DomainName, $DomainName.Split(".")[0], $DomainName.Split(".")[1]
+
 #Design the object
 $Props = @{
-    objectClass       = @("top", "dnsNode")
-    distinguishedName = "DC={0},DC={1},CN=MicrosoftDNS,DC=DomainDnsZones,DC={2},DC={3}" -f $DstRecord, $DomainName, $DomainName.Split(".")[0], $DomainName.Split(".")[1]
-    dnsRecord         = [System.Text.Encoding]::ASCII.GetString($dnsRecord)
+    objectClass = @("top", "dnsNode")
+    dnsRecord   = $dnsRecord.ToArray()
 }
 
-#Create the object according to design
-$obj = new-object PSObject -Property $Props
-
-#When dealing with password, LDAP server is likely
-#to require encrypted connection
-$Ldap = Get-LdapConnection -EncryptionType Kerberos
 #Create the object in directory
-$obj | Add-LdapObject -LdapConnection $Ldap
+$addRequest = New-Object System.DirectoryServices.Protocols.AddRequest($distinguishedName, $null)
+foreach ($key in $Props.Keys) {
+    $attribute = New-Object System.DirectoryServices.Protocols.DirectoryAttribute($key, $Props[$key])
+    $addRequest.Attributes.Add($attribute)
+}
+
+try {
+    $ldapConnection.SendRequest($addRequest)
+    Write-Host "DNS record added successfully."
+} catch {
+    Write-Error "Failed to add DNS record: $_"
+}
